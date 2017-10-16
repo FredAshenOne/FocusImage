c081e4260d82340c73a0659ae7ed528ab259465f
Ashen One <fredlam7@gmail.com>
2017-10-16 10:57:55 -0500

    scroll and transitions modified - last test

--- Js/main.js
+++ Js/main.js
@@ -1,23 +1,33 @@
 $(document).ready(function(){
 //
   $('.container').on('click',function(){
-    $('.container').addClass('container-enlarge');
-    $('.container').addClass('container-move');
+    $(this).addClass('container-enlarge');
+    $(this).addClass('container-move');
     $('#background').addClass('black-background');
-
+    $('#content').removeClass('hide-el');
+    $('#content').addClass('content-container');
   });
-$('.slicon').on('click',function(){
-  $('#content').addClass('display-el');
-});
 
   $('body').on('click',function(e){
     if (!$(e.target).closest('.container-enlarge').length) {
       $('#background').removeClass('black-background');
       $('.container').removeClass('container-enlarge');
       $('.container').removeClass('container-move');
-      $('#content').addClass('display-el');
-      $('#content').removeClass('par-container');
-      }
+      $('#content').addClass('hide-el');
+      $('#content').removeClass('content-container');
+      $('#icon-el').val('keyboard_arrow_left');
+    }
+  });
+
+  $('#arrow').on('click',function(){
+    if (!$('#content').hasClass('move-container')) {
+    $('#content').addClass('move-container');
+    console.log($('#icon-el').val());
+    $('#icon-el').val('keyboard_arrow_right');
+  }else{
+    $('#content').removeClass('move-container');
+    $('#icon-el').val('keyboard_arrow_left');
+  }
   });
   // $(document).scroll(function(){
   //   if ($('#black-background').hasClass('black-background')) {
